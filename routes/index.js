efec6d59e18b61f08ea30e08973aba2ce9313ef0
felipe300 <caracoloro@gmail.com>
2024-08-12 22:47:11 -0400

    feat: add hx-boost: form

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,11 @@ router.get("/articles/:id", (req, res) => {
   res.render("article", { title: article.name, article: article });
 });
 
+router.post("/articles", (req, res) => {
+  const { name, body } = req.body;
+  const newArticle = { id: Math.floor(Math.random() * 1000000), name, body };
+  articles.push(newArticle);
+  res.render("partials/list", { articles: articles });
+});
+
 export default router;
