6d978dfd9fe73b79af52ec5fade3774ee895075c
felipe300 <caracoloro@gmail.com>
2024-08-12 21:11:59 -0400

    feat: add list views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import express from "express";
+import articles from "../data/article.js";
 
 const router = express.Router();
 
 router.get("/", (_, res) => {
-  res.render("index", { title: "Produc Listing" });
+  res.render("index", { title: "Produc Listing", articles: articles });
 });
 
 export default router;
