70f1d88670d3423ab8a705a643758438263730ed
felipe300 <caracoloro@gmail.com>
2024-08-13 16:09:28 -0400

    feat: add loader

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,11 @@ router.post("/articles", (req, res) => {
   const { name, body } = req.body;
   const newArticle = { id: Math.floor(Math.random() * 1000000), name, body };
   articles.push(newArticle);
-  res.render("partials/list", { articles: articles });
+
+  // Fake delay to test `hx-indicator` and `hx-request`
+  setTimeout(() => {
+    res.render("partials/list", { articles: articles });
+  }, 3000);
 });
 
 export default router;
